cffd79c561a658e217bf721e591e41cb31de890b
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2019-07-07 22:31:44 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import Navbar from './components/layout/Navbar'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import { Provider } from './context';
+
+import Navbar from './components/layout/Navbar'
 import Index from './components/layout/Index'
+import Lyrics from './components/tracks/Lyrics'
+
 import './App.css';
 
 function App() {
@@ -14,6 +17,7 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={Index}/>
+              <Route exact path="/lyrics/track/:id" component={Lyrics}/>
             </Switch>
           </div>
         </React.Fragment>
