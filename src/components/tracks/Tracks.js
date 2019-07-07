5495552313f9c0f2d5df2774f76aee4bc7d41dec
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2019-07-07 21:38:11 +0200

    Update

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import {Consumer} from '../../context'
+import {Consumer} from '../../context';
+import Spinner from '../layout/Spinner';
 
 class Tracks extends Component {
     render() {
         return (
             <Consumer>
                 {value =>{
-                    console.log(value);
-                    return <h1>Tracks</h1>
+                    const {track_list, heading} = value;
+                    if( track_list ===undefined || track_list.length === 0){
+                        return <Spinner />
+                    } else {
+                        return(
+                            <React.Fragment>
+                                <h3 className="text-center mb-4">{heading}</h3>
+                            <div className="row">
+
+                            </div>
+                            </React.Fragment>
+                            
+                        )
+                    }
                 }}
             </Consumer>
         )
