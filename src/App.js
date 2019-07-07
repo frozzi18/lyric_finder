1b5b3beabca322438a97d4ce1c5c66d0a9db1fc7
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2019-07-07 20:05:31 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { Provider } from './context';
 import Index from './components/layout/Index'
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <React.Fragment>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Index}/>
-          </Switch>
-        </div>
-      </React.Fragment>
-    </Router>      
+    <Provider>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index}/>
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
+    </Provider>
+          
   );
 }
 
