e1dc27480bb1d87efa08d973e5d8aa0d7b7ed4b3
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2019-07-08 07:34:58 +0200

    Update context.js and Search.js

--- src/components/tracks/Search.js
+++ src/components/tracks/Search.js
@@ -7,8 +7,19 @@ class Search extends Component {
         trackTitle:''
     }
 
+    findTrack = (e) => {
+        e.preventDefault();
+
+        axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?q_track=${this.state.trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY}`)
+        .then(res => {
+            console.log(res.data);
+            // this.setState({track_list:res.data.message.body.track_list})
+        })
+        .catch(err => console.log(err))
+    }
+
     onChange = e => {
-        this.setState({trackTitle: e.target.value});
+        this.setState({[e.target.name]: e.target.value});
     }
 
 
@@ -16,13 +27,14 @@ class Search extends Component {
         return (
             <Consumer>
                 {value => {
+                    // console.log(value)
                     return (
                       <div className="card card-body mb-4 p-4">
                           <h1 className="display-4 text-center">
                               <i className="fas fa-music"></i> Search For A Song
                           </h1>
                           <p className="lead text-center">Get the lyrics for any song</p>
-                          <form>
+                          <form onSubmit={this.findTrack}>
                               <div className="form-group">
                                   <input type="text" 
                                   className="form-control form-control-lg" 
@@ -31,8 +43,10 @@ class Search extends Component {
                                   value={this.state.trackTitle}
                                   onChange={this.onChange}
                                   />
-
                               </div>
+                              <button className="btn btn-primary btn-lg btn-block mb-5" type="submit">
+                                  Get Track Lyrics
+                              </button>
                           </form>
                       </div>  
                     )
