e5614eb387f45610bb24870ed57b65b681005b0e
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2019-07-08 06:50:19 +0200

    Update Lyrics.js

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -18,6 +18,7 @@ class Lyrics extends Component {
             return axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`)
         })
         .then(res => {
+            console.log(res.data)
             this.setState({track:res.data.message.body.track})
         })
         .catch(err => console.log(err))          
@@ -32,10 +33,28 @@ class Lyrics extends Component {
                 <React.Fragment>
                     <Link to="/" className="btn btn-dark btn-sm mb-4">Go Back</Link>
                     <div className="card">
-                        <div className="card-header">
-                            {track.track_name} by 
+                        <h5 className="card-header">
+                            {track.track_name} by <span className="text-secondary">{track.artist_name}</span>
+                        </h5>
+                        <div className="card-body">
+                            <p className="card-text">{lyrics.lyrics_body}</p>
                         </div>
                     </div>
+
+                    <ul className="list-group mt-3">
+                        <li className="list-group-item">
+                            <strong>Album ID</strong>: {track.album_id}
+                        </li>
+                        <li className="list-group-item">
+                            <strong>Song Genre</strong>: {track.primary_genres.music_genre_list[0].music_genre.music_genre_name}
+                        </li>
+                        <li className="list-group-item">
+                            <strong>Explicit Words</strong>: {track.explicit === 0 ? 'No' : 'Yes'}
+                        </li>
+                        <li className="list-group-item">
+                            <strong>Release Date</strong>: {track.first_release_date}
+                        </li>
+                    </ul>
                 </React.Fragment>
             )
         }
